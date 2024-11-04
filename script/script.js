23e5cef1fe2727d0efb3032657f28d237d7840bd
Tobias982 <144317661+Tobias982@users.noreply.github.com>
2024-11-04 09:20:21 +0100

    fuckJS

---
+++ script/script.js
@@ -0,0 +1,7 @@
+let django=0;
+
+function fuckjs() {
+    django+=1;
+    let nig = document.getElementById("nnnn").value;
+    document.getElementById("fuckjs").innerHTML += django + '. ' +  nig + '<br>';
+}
